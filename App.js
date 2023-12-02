d2d0bd12917c0abda521257dd5e9292d7eb9b94a
AlphaRomeoMike <74718326+AlphaRomeoMike@users.noreply.github.com>
2023-12-02 16:59:23 +0500

    - Adding headers

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StyleSheet, View } from 'react-native';
 
 import Header from './components/Header';
-import { Login } from './components/Login';
+import { useState } from 'react';
+import { TextInput } from 'react-native-paper';
+import { updateState } from './utilities/helpers';
 
 export default function App() {
-  const title = 'Elastic Ecommerce'
+  const title = 'Elastic Ecommerce';
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  });
+
+
   return (
-      <View style={styles.container}>
-        <Header style={styles.header} title={title} />
+    <View style={styles.container}>
+      <Header style={styles.header} title={title} />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder='Email'
+          onChangeText={(text) => setState(email = text)}
+        ></TextInput>
       </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder='Password'
+          secureTextEntry
+          onChangeText={(text) => setState(email = text)}
+        ></TextInput>
+      </View>
+    </View>
   );
 }
 
@@ -23,6 +46,20 @@ const styles = StyleSheet.create({
     color: '#f2e9e4',
     marginTop: 30,
     fontSize: 30,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#e5e5e5",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
+  inputText: {
+    height: 50,
+    color: "white"
   },
 });
