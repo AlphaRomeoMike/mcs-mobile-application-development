c9a312431f7b07ff82ffae4e0b1b90a1708d0dbd
AlphaRomeoMike <74718326+AlphaRomeoMike@users.noreply.github.com>
2023-12-27 21:48:57 +0500

    - WIP todo listing

--- components/Todo.js
+++ components/Todo.js
@@ -1,13 +1,20 @@
-import { Text, View } from "react-native"
+import { Switch, Text, View } from "react-native"
+
+import theme from "../constants/theme";
+
+const { accent, yellow } = theme
 
 function Todo({ navigation, todo }) {
     return (
         <View>
-            <View>
+            <View style={{}}>
                 <Text>{todo ?? todo.title}</Text>
                 <Text>{todo ?? todo.description}</Text>
                 <Text>{todo ?? todo.category}</Text>
-                <Text>{todo ?? todo.status}</Text>
+                <Text><Switch
+                    trackColor={{ false: accent.toString(), true: yellow.toString() }}
+                    onValueChange={() => {}}
+                ></Switch></Text>
             </View>
         </View>
     )
