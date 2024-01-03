14d54d8094aa8bcc8834fd5ac043a0529ef2388d
AlphaRomeoMike <74718326+AlphaRomeoMike@users.noreply.github.com>
2024-01-03 21:04:55 +0500

    - Set seeder

--- App.js
+++ App.js
@@ -6,21 +6,40 @@ import theme from '@constants/theme';
 import Todos from '@components/Todos';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
+import { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import data from './helpers/data';
 const Stack = createNativeStackNavigator();
 
 const { accent, background, white, lightyellow, yellow, support } = theme;
 
 function App() {
+
+  useEffect(() => {
+    let seed = async () => {
+      if (__DEV__) {
+        let prop = await AsyncStorage.getItem('user');
+        if (!prop) {
+          await AsyncStorage.setItem('user', JSON.stringify({ username: "Expo", email: "expo@example.com", password: "Pa$$w0rd" }));
+          await AsyncStorage.setItem('user_todos', JSON.stringify(data));
+
+          console.log(`Seeding completed`);
+        }
+      }
+    }
+
+    seed()
+  });
+
   const title = 'Elastic Todo';
   return (
     <NavigationContainer >
       <View style={styles.container}>
         <Stack.Navigator initialRouteName='Login' screenOptions={{
           statusBarColor: background,
-          headerStyle: {backgroundColor: background},
+          headerStyle: { backgroundColor: background },
           headerTintColor: white
-          }}>
+        }}>
           <Stack.Screen name='Login' component={Login} />
           <Stack.Screen name="Signup" component={Signup} />
           <Stack.Screen name="Todos" component={Todos} />
@@ -31,7 +50,7 @@ function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {  
+  container: {
     flex: 1,
     backgroundColor: background.toString(),
     justifyContent: 'center',
